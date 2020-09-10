d395f369659742ec08efbf8181e7f6c973246bd0
Ajay Yamkar <58873896+Ajyamkar@users.noreply.github.com>
2020-09-10 20:47:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function shareFunction(){
+  document.getElementById("share").src="images/icon-facebook.svg";
+  // document.getElementById("share").src="images/icon-pinterest.svg";
+  // document.getElementById("share").src="images/icon-twitter.svg";
+  // document.getElementById("share").style="background-color:yellow";
+}
